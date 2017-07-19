572de2143dcc1c67ff6e8f3ad61846a4cc5bde8f
iBrie <ibrigitte@me.com>
2017-07-19 17:56:51 +0200

    Protocols

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -7,20 +7,45 @@
 //
 
 
-class BankVault {
-    
+
+protocol ProvideAccess {
+  func allowEntryWithPassword(password: [Int]) -> Bool
+  }
+
+
+
+
+class BankVault : ProvideAccess {
+  
     let name: String
     let address: String
     var amount: Double = 0.0
+  
+  
     
     init(name: String, address: String) {
         self.name = name
         self.address = address
     }
     
+  }
+
+
+
+extension BankVault {
+  func allowEntryWithPassword(password: [Int]) -> Bool {
+    guard !password.isEmpty && password.count <= 10 else {return false}
+    
+    for (index , digit) in password.enumerated(){
+    guard index % 2 == 0 else {
+      continue
+    }
+      if digit % 2 != 0 {
+        return false
+      }
+    }
+    return true
+  }
 }
-
-
-
 
 
--- Protocols/Ships.swift
+++ Protocols/Ships.swift
@@ -7,7 +7,34 @@
 //
 
 
+enum Direction {
+  case  north,
+        south,
+        east,
+        west
+  }
 
+protocol SteerAbility {
+  var direction : Direction {get set
+   }
+ mutating func steerInDirection(direction:Direction)
+  
+  }
 
+extension SteerAbility {
+  mutating func steerInDirection(direction:Direction){
+    self.direction = direction  }
 
+}
 
+class SailBoat : SteerAbility{
+  var direction : Direction = .north
+}
+
+class RowBoat: SteerAbility {
+  var direction : Direction = .north
+}
+
+class SteamBoat: SteerAbility {
+  var direction: Direction = .north
+}
